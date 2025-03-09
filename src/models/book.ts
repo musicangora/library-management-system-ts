da2e7ef76a469d2113820e4e894c31980778cc8e
S-ima7 <knotonlybutalso@gmail.com>
2025-03-09 16:49:45 +0900

    フォーマットの修正

--- src/models/book.ts
+++ src/models/book.ts
@@ -1,29 +1,30 @@
 // src/models/Book.ts
 
-/**
- * Bookインターフェイス
- * JPAのエンティティをTypeScriptの型定義だけで置き換えたもの
- */
 export interface Book {
-    /** 主キー */
-    id?: number; // DBが自動採番するなら省略可能にしておくなど
-  
-    /** タイトル（必須項目） */
-    title: string;
-  
-    /** 著者名（必須項目） */
-    author: string;
-  
-    /** 出版社名（必須項目） */
-    publisher: string;
-  
-    /** 分類（必須項目） */
-    category: string;
-  
-    /** 在庫数（必須項目） */
-    quantity: number;
-  
-    /** ISBNコード */
-    isbn: string;
-      }
-  
+  /** 主キー */
+  id?: number; // DBが自動採番するなら省略可能にしておくなど
+
+  /** タイトル（必須項目） */
+  title: string;
+
+  /** 著者名（必須項目） */
+  author: string;
+
+  /** 出版社名（必須項目） */
+  publisher: string;
+
+  /** 分類（必須項目） */
+  category: string;
+
+  /** 在庫数（必須項目） */
+  quantity: number;
+
+  /** ISBNコード（ユニーク制約） */
+  isbn: string;
+
+  /** 出版年 */
+  publicationYear?: number;
+
+  /** 新しく追加するフィールド（在庫数） */
+  stock: number;
+}
