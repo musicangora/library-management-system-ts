ec13ef45ebe839d1056b9521404e1678b968f596
S-ima7 <knotonlybutalso@gmail.com>
2025-03-09 19:08:28 +0900

    feat: user.ts の追加

---
+++ src/models/user.ts
@@ -0,0 +1,34 @@
+/** Roleの列挙 */
+export enum Role {
+    ADMIN = "ADMIN",
+    USER = "USER",
+    GUEST = "GUEST",
+  }
+  
+  /** Userインターフェイス */
+  export interface User {
+    /** 主キー */
+    id: number; 
+  
+    /** ユーザー名 */
+    username: string;
+  
+    /** パスワード */
+    password: string;
+  
+    /** 有効フラグ */
+    enabled: boolean;
+  
+    /** ロック状態フラグ */
+    accountNonLocked: boolean;
+  
+    /** 失敗回数 */
+    failedAttempt: number;
+  
+    /** ロックされた日時 */
+    lockTime?: Date; // Date型で表す
+  
+    /** ユーザーの権限（JavaではSet<Role>だった部分） */
+    roles: Set<Role>; // Set<Role>型で表す
+  }
+  
